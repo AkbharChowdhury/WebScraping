88943cf720b5e77d90a2a722901a74941600ce1e
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-06-11 09:11:47 +0100

    a

--- news_scrape.py
+++ news_scrape.py
@@ -18,6 +18,7 @@ def is_weekend(d=datetime.today()):
 
 class News(BaseModel):
     __container: str = 'container-more-features'
+
     def __get_news_data(self):
         with requests.get('https://www.theguardian.com/uk') as response:
             soup: BeautifulSoup = BeautifulSoup(response.content, 'html.parser')
@@ -37,25 +38,6 @@ class News(BaseModel):
                     pass
 
 
-def fetch_articles(container_div_id: str = 'container-more-features'):
-    soup = get_news_data()
-    container = soup.find(id=container_div_id)
-    uls = container.find_all('ul')
-    for ul in uls:
-        for li in ul:
-            try:
-                category: str = li.find(class_='card-headline').find('div').text
-                title: str = li.find_all('div')[1].find('a')['aria-label']
-                yield dict(title=title, featured_text=category)
-            except Exception:
-                pass
-
-
-def show_article_details(title: str):
-    news = list(fetch_articles(title))
-    print(json.dumps(news, indent=4))
-
-
 def main():
     tag: str = Tags.WEEKEND.name if is_weekend() else Tags.SPORT.name
     news = News(__container=tag)
@@ -63,5 +45,6 @@ def main():
 
 
 if __name__ == '__main__':
+    # print(Tags('MORE'))
 
     main()
